2b5f955852b1d060dd073075d1547db13e3f61c2
2AM-official <zhaoxinglyu@icloud.com>
2022-05-09 00:10:37 -0700

    finish baseline

--- A4_256_sp22/count_freqs.py
+++ A4_256_sp22/count_freqs.py
@@ -123,11 +123,18 @@ class Hmm(object):
                 output.write("%i %i-GRAM %s\n" %(self.ngram_counts[n-1][ngram], n, ngramstr))
 
     def replace_infrequent(self):
-        emission_copy = copy.copy(self.emission_counts)
-        for word, ne_tag in emission_copy:
-            if self.emission_counts[(word, ne_tag)] < 5:
-                self.emission_counts[("_RARE_", ne_tag)]= self.emission_counts.get(("_RARE_", ne_tag), 0)+self.emission_counts[(word, ne_tag)]
-                del self.emission_counts[(word, ne_tag)]
+        emission_copy = copy.deepcopy(self.emission_counts)
+        for word, _ in emission_copy:
+            gene_count = self.emission_counts.get((word, "I-GENE"), 0)
+            o_count = self.emission_counts.get((word, "O"), 0)
+            if gene_count + o_count < 5:
+                count += 1
+                self.emission_counts[("_RARE_", "I-GENE")] = self.emission_counts.get(("_RARE_", "I-GENE"), 0)+gene_count
+                self.emission_counts[("_RARE_", "O")] = self.emission_counts.get(("_RARE_", "O"), 0)+o_count
+                if (word, "I-GENE") in self.emission_counts:
+                    del self.emission_counts[(word, "I-GENE")]
+                if (word, "O") in self.emission_counts:
+                    del self.emission_counts[(word, "O")]
     
     def improve_baseline(self):
         emission_copy = copy.copy(self.emission_counts)
@@ -184,10 +191,63 @@ class Hmm(object):
                 else:
                     output_corpus.write(word + " " + rare_type +"\n")
             line = corpus.readline()
+
+
+    def dev_improve(self, corpus, output_corpus):
+        zero_count = 0
+        gene_count = 0
+        for word, ne_tag in self.emission_counts:
+            if ne_tag == "I-GENE":
+                gene_count += self.emission_counts[(word, ne_tag)]
+            else:
+                zero_count += self.emission_counts[(word, ne_tag)]
+        rare_gene = self.emission_counts[("_RARE_", "I-GENE")]/gene_count
+        rare_zero = self.emission_counts[("_RARE_","O")]/zero_count
+        rare_type = ""
+        if rare_gene >= rare_zero:
+            rare_type = "I-GENE"
+        else:
+            rare_type = "O"
+
+        few_gene = self.emission_counts[("_FEW_", "I-GENE")]/gene_count
+        few_zero = self.emission_counts[("_FEW_","O")]/zero_count
+        few_type = ""
+        if few_gene >= few_zero:
+            few_type = "I-GENE"
+        else:
+            few_type = "O"
+        line = corpus.readline()
+        while line:
+            if line:
+                fields = line.split(" ")
+                word = fields[0].strip('\n')
+                #print(word)
+                if not len(word):
+                    output_corpus.write("\n")
+                    line = corpus.readline()
+                    continue
+                word_gene = 0
+                word_zero = 0
+                if (word, "I-GENE") in self.emission_counts and (word,'O') in self.emission_counts:
+                    word_gene = self.emission_counts[(word, "I-GENE")]/gene_count
+                    word_zero = self.emission_counts[(word,"O")]/zero_count
+                    word_type = ""
+                    if word_gene >= word_zero:
+                        word_type = "I-GENE"
+                    else:
+                        word_type = "O"
+                    output_corpus.write(word + " " + word_type +"\n")
+                elif (word, "I-GENE") in self.emission_counts:
+                    word_type = "I-GENE"
+                    output_corpus.write(word + " " + word_type +"\n")
+                elif (word,'O') in self.emission_counts:
+                    word_type = "O"
+                    output_corpus.write(word + " " + word_type +"\n")
+                else:
+                    output_corpus.write(word + " " + rare_type +"\n")
+            line = corpus.readline()
+
     
-
-
-
 
     def read_counts(self, corpusfile):
 
@@ -235,12 +295,12 @@ if __name__ == "__main__":
     # Collect counts
     counter.train(input)
     # replace infrequent words
-    #counter.replace_infrequent()
+    counter.replace_infrequent()
     # replace improve infrequent words
-    counter.improve_baseline()
+    #counter.improve_baseline()
     # Write the counts
-    counter.write_counts(sys.stdout)
-    #counter.dev_output(dev, sys.stdout)
+    #counter.write_counts(sys.stdout)
+    counter.dev_output(dev, sys.stdout)
     
 
 
